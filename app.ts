3f604682801a6ddefbb876132c5453e9a7486926
Aditya Kumar <adityaverma.dev@gmail.com>
2021-10-16 23:03:47 +0530

    static data added

--- app.ts
+++ app.ts
@@ -1,15 +1,18 @@
-import fastify from 'fastify'
+import fastify from 'fastify';
+import bmiRoute from './api/bmi/index';
 
-const server = fastify()
+const server = fastify();
+
+server.register(bmiRoute);
 
 server.get('/ping', async (request, reply) => {
-  return 'pong\n'
-})
+  return 'pong\n';
+});
 
 server.listen(8080, (err, address) => {
   if (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   }
-  console.log(`Server listening at ${address}`)
+  console.log(`Server listening at ${address}`);
 });
